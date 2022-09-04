41dc242f4b54cba8284a59a186b8bc6c80939907
AdityaTajanpure <adityat@accucia.co>
2022-09-04 18:47:49 +0530

    solved mobile connection issue

--- client/src/TypingMessage/TypingMessage.js
+++ client/src/TypingMessage/TypingMessage.js
@@ -1,17 +1,18 @@
 import React from "react";
 import TypingAnimation from "../TypingAnimation/TypingAnimation";
-
+import Gravatar from "react-gravatar";
 import "./TypingMessage.css";
 
 const TypingMessage = ({ user }) => {
   return (
     <div className="message-item">
       <div className="message-avatar-container">
-        <img
-          src={user.picture}
+        <Gravatar
+          email={user.email ?? user.name + "@gmail.com"}
           alt={user.name}
-          className={"message-avatar"}
-        ></img>
+          title={user.name}
+          className={"avatar"}
+        ></Gravatar>
       </div>
 
       <TypingAnimation></TypingAnimation>
